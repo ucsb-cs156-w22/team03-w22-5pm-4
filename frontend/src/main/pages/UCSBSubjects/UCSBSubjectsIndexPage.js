eae4c9f59457e87bc0500e107093aa316ffb8a65
zjw963546 <xsdzhengjianwei@163.com>
2022-02-22 17:24:22 -0800

    jz-add delete

--- frontend/src/main/pages/UCSBSubjects/UCSBSubjectsIndexPage.js
+++ frontend/src/main/pages/UCSBSubjects/UCSBSubjectsIndexPage.js
@@ -1,13 +1,27 @@
+import React from 'react'
+import { useBackend } from 'main/utils/useBackend';
+
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import UCSBSubjectsTable from 'main/components/UCSBSubjects/UCSBSubjectsTable';
+import { useCurrentUser } from 'main/utils/currentUser'
 
 export default function UCSBSubjectsIndexPage() {
+
+  const currentUser = useCurrentUser();
+
+  const { data: subjects, error: _error, status: _status } =
+    useBackend(
+      // Stryker disable next-line all : don't test internal caching of React Query
+      ["/api/UCSBSubjects/all"],
+      { method: "GET", url: "/api/UCSBSubjects/all" },
+      []
+    );
+
   return (
     <BasicLayout>
       <div className="pt-2">
         <h1>UCSBSubjects</h1>
-        <p>
-          This is where the index page will go
-        </p>
+        <UCSBSubjectsTable subjects={subjects} currentUser={currentUser} />
       </div>
     </BasicLayout>
   )
