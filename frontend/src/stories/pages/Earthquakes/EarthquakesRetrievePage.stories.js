76c0dd28ce426f05dc48ace9a78edbff68a0002f
zjw963546 <xsdzhengjianwei@163.com>
2022-02-27 16:49:38 -0800

    change submit to retrieve in storybook

---
+++ frontend/src/stories/pages/Earthquakes/EarthquakesRetrievePage.stories.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+
+import EarthquakesRetrievePage from "main/pages/Earthquakes/EarthquakesRetrievePage";
+
+export default {
+    title: 'pages/Earthquakes/EarthquakesRetrievePage',
+    component: EarthquakesRetrievePage
+};
+
+const Template = () => <EarthquakesRetrievePage />;
+
+export const Default = Template.bind({});
+
+
+
+
